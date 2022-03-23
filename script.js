6b3726a8efa17c0f84f07dfd104e7c3f50324984
itsrasfa <rafaelacontatoss@gmail.com>
2022-03-23 18:46:31 -0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// máquina de escrever
 function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split('')
   elemento.innerHTML = '';
@@ -10,8 +11,21 @@ const titulo = document.querySelector("h1");
 typeWriter(titulo)
 typeWriter(document.querySelector(".texto"));
 
-
+// ativar o tema light
 const check = document.getElementById("check");
 check.addEventListener('change', () => {
   document.body.classList.toggle('light');
 })
+
+// ativar scroll 
+const btnBackToTop = document.querySelector("#back-to-top");
+btnBackToTop.addEventListener("click", (e) => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+window.addEventListener("scroll", (e) => {
+  btnBackToTop.style.display = window.scrollY > 20 ? "flex" : "none";
+});
